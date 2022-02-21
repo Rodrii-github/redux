832edc59852727435ea78046af4499d4fe66592d
Rodrigo <73354578+Rodrii-github@users.noreply.github.com>
2022-02-21 15:24:36 -0300

    contador react-redux

--- src/index.js
+++ src/index.js
@@ -1,39 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
-import App from "./App";
+import { Provider } from "react-redux";
+import App, { reducer } from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const store = createStore((state = 0, action) => {
-  // es un reducer!
-  switch (action.type) {
-    case "incrementar": {
-      return state + 1;
-    }
-    case "decrementar": {
-      return state - 1;
-    }
-    case "set": {
-      return action.payload;
-    }
-    default:
-      return state;
-  }
-});
-
-console.log(store.getState());
-store.dispatch({ type: "lala" });
-console.log(store.getState());
-store.dispatch({ type: "incrementar" });
-console.log(store.getState());
-store.dispatch({ type: "decrementar" });
-console.log(store.getState());
-store.dispatch({ type: "set", payload: 15 });
-console.log(store.getState());
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
